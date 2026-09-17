1604f602d2b7555321448badbbdbe9e6059dc9d0
Md. Saifur Rahman Shadhin <mdsaifurr944@gmail.com>
2026-09-17 19:35:15 +0600

    Add user creation API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class User(BaseModel):
+    name: str
+    email: str
+    age: int
+
+@app.post("/users")
+def create_user(user: User):
+    return user
 
 @app.get("/")
 def root():
