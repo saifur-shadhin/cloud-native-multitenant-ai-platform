6b3da99c3c76154a555aa7fb1de6f17fe7d47ff8
Md. Saifur Rahman Shadhin <mdsaifurr944@gmail.com>
2026-09-19 22:06:22 +0600

    Complete FastAPI CRUD API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,67 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+app=FastAPI()
+users = []
 
 class User(BaseModel):
+    id: int
     name: str
     email: str
-    age: int
+    age: int = Field(ge=1, le=100)
+
+# GET all users
+@app.get("/users")
+def get_users():
+    return users
+
 
 @app.post("/users")
 def create_user(user: User):
+    for existing_user in users:
+        if existing_user.id == user.id:
+            raise HTTPException(
+                status_code=400,
+                detail="User ID already exists"
+            )
+
+    users.append(user)
     return user
 
+# GET single user
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return user
+
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+# PUT - update user
+@app.put("/users/{user_id}")
+def update_user(user_id: int, updated_user: User):
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            users[index] = updated_user
+            return updated_user
+
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+# DELETE - delete user
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            deleted_user = users.pop(index)
+            return deleted_user
+
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+# Root
 @app.get("/")
 def root():
-    return {"message": "Cloud-Native Multi-Tenant AI Platform is running!"}
+    return {
+        "message": "Cloud-Native Multi-Tenant AI Platform is running!"
+    }
